5e94e37138983085419ca309843282255af245b8
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-10-30 17:43:34 +0300

    add router

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const AppRouter = () => {
+  const auth = true
+  return (
+    auth === true 
+    ?
+    <Routes>
+      
+    </Routes>
+    :
+    <Routes>
+      
+    </Routes>
+  );
+};
+
+export default AppRouter;
